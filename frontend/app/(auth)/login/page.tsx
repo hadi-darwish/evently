57d51d3477207ec0f1c0f52f4c830d3be2c0d01c
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-25 23:18:34 +0300

    feat(frontend): add login auth route page

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@/auth";
 import { LoginForm } from "@/components/auth/login-form";
+import { useRouter } from "next/navigation";
+import { NextResponse } from "next/server";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  // const session = await auth();
+  // session && NextResponse.redirect("/settings");
   return <LoginForm />;
 };
 
