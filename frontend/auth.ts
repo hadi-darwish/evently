6320430ac71f03ca30355591069c70af3f2d6870
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-25 23:15:00 +0300

    feat(frontend): add auth handlers

---
+++ frontend/auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import authConfig from "./auth.config";
+import PostgresAdapter from "@auth/pg-adapter";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PostgresAdapter(pool),
+  ...authConfig,
+});
