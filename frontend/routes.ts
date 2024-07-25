33000c2fd666396d8b5e0bdcd45b53721ae064be
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-25 23:16:09 +0300

    feat(frontend): add main routes

---
+++ frontend/routes.ts
@@ -0,0 +1,38 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes will redirect logged in users to the home page
+ * @type {string[]}
+ */
+export const authRoutes = [
+  "/login",
+  "/register",
+  "/forgot-password",
+  "/reset-password",
+];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for authentication
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after a successful login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/";
+
+/**
+ * An array of routes that are ignored by the authentication middleware
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const ignoredRoutes = ["/api/weebhook/stripe", "/api/uploadthing"];
