c4c8ce31ec30815014d6c469bf6c4d14e4c5fbd7
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-25 23:22:03 +0300

    feat(frontend): edit header component

--- frontend/components/shared/Header.tsx
+++ frontend/components/shared/Header.tsx
@@ -1,10 +1,11 @@
-import AuthButton from "@/app/api/[...nextauth]/AuthButton.server";
 import Image from "next/image";
 import Link from "next/link";
 import { LoginButton } from "../auth/login-button";
 import { Button } from "../ui/button";
+import { auth, signOut } from "@/auth";
 
-const Header = () => {
+const Header = async () => {
+  const session = await auth();
   return (
     <header className="w-full border-b ">
       <div className="wrapper flex items-center justify-between">
@@ -17,14 +18,30 @@ const Header = () => {
           />
         </Link>
 
-        <div className="flex w-32 justify-end gap-3">
-          <LoginButton>
-            {/* <AuthButton /> */}
-            <Button className="rounded-full" size="lg">
-              Login
-            </Button>
-          </LoginButton>
-        </div>
+        {session !== null && (
+          <div className="flex w-32 justify-end gap-3">
+            <form
+              action={async () => {
+                "use server";
+                await signOut();
+              }}
+            >
+              <Button type="submit" className="rounded-full" size="lg">
+                Logout
+              </Button>
+            </form>
+          </div>
+        )}
+
+        {!session && (
+          <div className="flex w-32 justify-end gap-3">
+            <LoginButton>
+              <Button className="rounded-full" size="lg">
+                Login
+              </Button>
+            </LoginButton>
+          </div>
+        )}
       </div>
     </header>
   );
