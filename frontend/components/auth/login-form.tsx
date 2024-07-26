6c5063d37ad55fd2fd6eb1d10f23ebc0d56ab6b0
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-26 19:25:19 +0300

    feat(frontend): fix some paths

--- frontend/components/auth/login-form.tsx
+++ frontend/components/auth/login-form.tsx
@@ -19,7 +19,7 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FormError } from "../form-error";
 import { FormSuccess } from "../form-success";
-import { login } from "@/actions/login";
+import { login } from "@/lib/actions/login";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
