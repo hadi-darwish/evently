cb05e73d3ecc53b78d474505d960cf12e85d82a4
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-26 19:32:26 +0300

    feat(frontend): add createCategory and  getAllCategories actions

---
+++ frontend/lib/actions/category.actions.ts
@@ -0,0 +1,55 @@
+"use server";
+
+import { CreateCategoryParams } from "@/types";
+import { handleError } from "../utils";
+import client from "@/apolloClient";
+import {
+  CategoriesConnection,
+  CreateCategoryInput,
+  CreateCategoryPayload,
+  QueryAllCategoriesArgs,
+} from "@/schemas/generated/graphql";
+import {
+  CREATE_CATEGORY_MUTATION,
+  GET_ALL_CATEGORIES_QUERY,
+} from "../mutations/categories";
+
+export const createCategory = async ({
+  categoryName,
+}: CreateCategoryParams) => {
+  try {
+    const input: CreateCategoryInput = {
+      category: {
+        name: categoryName,
+      },
+    };
+    const { data } = await client.mutate<{
+      createCategory: CreateCategoryPayload;
+    }>({
+      mutation: CREATE_CATEGORY_MUTATION,
+      variables: { input },
+    });
+    console.log(data?.createCategory?.category);
+
+    return data?.createCategory?.category;
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getAllCategories = async (args: QueryAllCategoriesArgs) => {
+  try {
+    const { data } = await client.query<{
+      allCategories: CategoriesConnection;
+    }>({
+      query: GET_ALL_CATEGORIES_QUERY,
+      variables: args,
+    });
+
+    console.log(data?.allCategories?.edges.map((edge) => edge.node));
+
+    return data?.allCategories?.edges.map((edge) => edge.node);
+  } catch (error) {
+    handleError(error);
+  }
+};
