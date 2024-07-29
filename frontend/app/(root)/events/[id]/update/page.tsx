28197215646c540f34f908877fe19d7386f4db94
hadi-darwish <hadi.darwish.03@gmail.com>
2024-07-29 19:50:37 +0300

    feat(frontend): add update event form

--- frontend/app/(root)/events/[id]/update/page.tsx
+++ frontend/app/(root)/events/[id]/update/page.tsx
@@ -1,11 +1,19 @@
 import { auth } from "@/auth";
 import EventForm from "@/components/shared/EventForm";
+import { getEventById } from "@/lib/actions/events.actions";
 
-const UpdateEvent = async () => {
+type UpdateEventProps = {
+  params: {
+    id: string;
+  };
+};
+
+const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
   const session = await auth();
 
   //@ts-ignore
   const userId = session?.user?.user_id as string;
+  const event = await getEventById(Number(id));
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -15,7 +23,12 @@ const UpdateEvent = async () => {
       </section>
 
       <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" />
+        <EventForm
+          type="Update"
+          event={event}
+          eventId={event?.id}
+          userId={userId}
+        />
       </div>
     </>
   );
