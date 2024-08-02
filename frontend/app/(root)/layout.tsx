540c88e907f886189f63da81476c8fb93208783b
hadi-darwish <hadi.darwish.03@gmail.com>
2024-08-03 01:21:00 +0300

    hotfix(frontend): add session provider

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,16 +1,21 @@
+import { auth } from "@/auth";
 import Footer from "@/components/shared/Footer";
 import Header from "@/components/shared/Header";
+import { SessionProvider } from "next-auth/react";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
-    <div className="flex h-screen flex-col">
-      <Header />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
+    <SessionProvider session={session}>
+      <div className="flex h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </SessionProvider>
   );
 }
