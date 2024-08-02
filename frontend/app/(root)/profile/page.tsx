ee71cbc26d85218f83d673f6fe69312d3af55b0b
hadi-darwish <hadi.darwish.03@gmail.com>
2024-08-03 01:22:15 +0300

    feat(frontend): add content of profile page

--- frontend/app/(root)/profile/page.tsx
+++ frontend/app/(root)/profile/page.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { auth } from "@/auth";
 import { getEventsByUserId } from "@/lib/actions/events.actions";
 import { SearchParamProps } from "@/types";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const session = await auth();
@@ -12,11 +13,16 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const userId = session?.user?.user_id;
   //@ts-ignore
   const isOrganizer = session?.user?.organizerInfo !== null;
-
-  // const orders = await getOrdersByUserId(userId);
-
-  // const orderedEvents = orders?.data.map((order) => order?.eventByEventsId);
   const eventsPage = Number(searchParams?.eventsPage) || 1;
+
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+
+  const orders = !isOrganizer
+    ? await getOrdersByUser({ userId, page: ordersPage })
+    : null;
+
+  const orderedEvents =
+    orders?.data.map((order) => order?.eventByEventId) || [];
 
   let organizedEvents;
   if (isOrganizer) {
@@ -29,27 +35,31 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   return (
     <>
       {/* My Tickets */}
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-          <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">Explore More Events</Link>
-          </Button>
-        </div>
-      </section>
+      {!isOrganizer && (
+        <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+          <div className="wrapper flex items-center justify-center sm:justify-between">
+            <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
+            <Button asChild size="lg" className="button hidden sm:flex">
+              <Link href="/#events">Explore More Events</Link>
+            </Button>
+          </div>
+        </section>
+      )}
 
-      <section className="wrapper my-8">
-        {/* <Collection
-          data={orderedEvents}
-          emptyTitle="No event tickets purchased yet"
-          emptyStateSubtext="No worries - plenty of exciting events to explore!"
-          collectionType="My_Tickets"
-          limit={3}
-          page={ordersPage}
-          urlParamName="ordersPage"
-          totalPages={orders?.totalPages}
-        /> */}
-      </section>
+      {!isOrganizer && (
+        <section className="wrapper my-8">
+          <Collection
+            data={orderedEvents}
+            emptyTitle="No event tickets purchased yet"
+            emptyStateSubtext="No worries - plenty of exciting events to explore!"
+            collectionType="My_Tickets"
+            limit={3}
+            page={ordersPage}
+            urlParamName="ordersPage"
+            totalPages={orders?.totalPages}
+          />
+        </section>
+      )}
 
       {/* Events Organized */}
       {isOrganizer && (
